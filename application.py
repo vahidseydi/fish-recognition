e13431f87ae14cd2d58b68aa5145c0bcfd921eb0
Vahid Seydi <seydi.vahid@gmail.com>
2021-03-22 20:00:23 +0000

    Add files via upload

--- application.py
+++ application.py
@@ -8,21 +8,50 @@
 # ----------------------------------------------
 from flask import Flask, render_template
 import boto3
+import dotenv
+import os
+
+aws_access_key_id      = os.gevent('aws_access_key_id')
+aws-secret-access-key  = os.gevent('aws-secret-access-key')
+aws-region ='eu-west-2'
+S3_BUCKET='fish-classification-bucket'
 
 application = Flask(__name__)
 
 
-@application.route("/")
-def root():
-    return render_template("index.html")
-
-@application.route("/help")
-def helppage():
-    return render_template("help.html")
-
-@application.route("/hello")
+@app.route('/', methods=['GET'])
 def index():
-    return "Hello World from Flask Hello Page.<b> v1.0"
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to .s3/<bucetname>/test
+        s3_resource = boto3.resource('s3')
+        Key = 'test/{}'.format(secure_filename(f.filename))
+        s3_resource.Bucket(S3_BUCKET).put_object(Key=Key,Body=f)
+        
+        # Make prediction
+       
+        '''
+        client=boto3.client('rekognition','eu-west-2')
+        response = client.detect_labels(Image={'S3Object':{'Bucket':S3_BUCKET,'Name':Key}},
+        MaxLabels=10)
+       
+
+        print(str(len(response['Labels'])))
+        result = get_result(response)               # Convert to string
+        return result
+        '''
+        return "hello"
+       
+
+    return None
 
 #--------Main------------------
 if __name__ == "__main__":
